0ba47863a072bd9ec944afa8215a8a7672159b92
Rohit Pal <rohitpal440@gmail.com>
2017-08-20 22:11:02 +0530

    KadanesAlgorithm: move code to function

--- departure/array/KadanesAlgorithm.cpp
+++ departure/array/KadanesAlgorithm.cpp
@@ -8,8 +8,7 @@
 
 using namespace std;
 
-int main() {
-    vector<int> array = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+pair<int, pair<size_t, size_t> > getContigousMaxSum(const vector<int> &array) {
     pair<int, pair<size_t, size_t> > result = make_pair(array[0], make_pair(0, 0));
     pair<int, pair<size_t, size_t> > tempResult = make_pair(array[0], make_pair(0, 0));
     for (size_t i = 1; i < array.size(); i++) {
@@ -23,7 +22,14 @@ int main() {
             result = tempResult;
         }
     }
+    return result;
+}
+
+int main() {
+    vector<int> array = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    pair<int, pair<size_t, size_t> > result = getContigousMaxSum(array);
     cout << "Maximum sum: " << result.first << endl;
+    cout << "Sub-Array : ";
     for (auto i = result.second.first; i <= result.second.second; i++)
         cout << array[i] << " ";
     cout << endl;
