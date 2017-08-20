b01b475c63dcd83a4dda725eec572773a4db61d5
Rohit Pal <rohitpal440@gmail.com>
2017-08-20 21:35:05 +0530

    Add implementation of Kadane's Algorithm

---
+++ departure/array/KadanesAlgorithm.cpp
@@ -0,0 +1,31 @@
+/***
+ Kadane's Algorithm:
+ Given an array containing both negative and positive integers.
+ Find the contiguous sub-array with maximum sum.
+ ***/
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<int> array = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    pair<int, pair<size_t, size_t> > result = make_pair(array[0], make_pair(0, 0));
+    pair<int, pair<size_t, size_t> > tempResult = make_pair(array[0], make_pair(0, 0));
+    for (size_t i = 1; i < array.size(); i++) {
+        if (array[i] > tempResult.first + array[i]) {
+            tempResult = make_pair(array[i], make_pair(i, i));
+        } else {
+            tempResult.first = tempResult.first + array[i];
+            tempResult.second.second = i;
+        }
+        if (tempResult.first > result.first) {
+            result = tempResult;
+        }
+    }
+    cout << "Maximum sum: " << result.first << endl;
+    for (auto i = result.second.first; i <= result.second.second; i++)
+        cout << array[i] << " ";
+    cout << endl;
+    return 0;
+}
