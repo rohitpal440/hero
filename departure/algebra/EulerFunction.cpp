7063c74d8957f3a4a5bbbc5676b45b1d60259234
Rohit Pal <rohitpal440@gmail.com>
2018-04-11 00:46:37 +0530

    Fix bug and add documentations
    Fix indentation

--- departure/algebra/EulerFunction.cpp
+++ departure/algebra/EulerFunction.cpp
@@ -1,4 +1,9 @@
     // Euler Function ϕ(n) = No. of number which are relatively prime to n
+    // ϕ(p^a) = p^a - p^(a - 1) // where p is prime and a is natural number
+    // ϕ(ab) = ϕ(a).ϕ(b)
+    // n = p1^a1 . p2^a2 . p3^a3.....pk^ak
+    // using above equations we can drive
+    // ϕ(n) = n.(1 - 1/p1).(1 - 1/p2).....(1 - 1/pk)
     // Time Complexity: O(√n)
 #include <iostream>
 using namespace std;
@@ -6,15 +11,15 @@ using namespace std;
 int ϕ(int n) {
     int result = n;
     for (int i = 2; i*i <= n; i++) {
-        if (!(n & 1)) {
-            while (!(n & 1))
+        if (n % i == 0) {
+            while (n % i == 0)
                 n = n/i;
-                result = result - result/i;
-                }
+            result = result - result/i; // similar to result *= 1.0 - 1.0/i
+        }
     }
     if (n > 1)
         result = result - result/n;
-        return result;
+    return result;
 }
 
 int main() {
