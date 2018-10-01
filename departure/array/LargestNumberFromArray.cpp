af8e24f5fd7b9859b2ecb16e3dee7c4f9fce61a2
Rohit Pal <rohitpal440@gmail.com>
2018-10-01 17:19:57 +0530

    Improve Logic

--- departure/array/LargestNumberFromArray.cpp
+++ departure/array/LargestNumberFromArray.cpp
@@ -1,10 +1,10 @@
 /***
-* Given an array of numbers, arrange them in a way that yields the largest
-* value. For example, if the given numbers are {54, 546, 548, 60}, the
-* arrangement 6054854654 gives the largest value. And if the given number
-* are {1, 34, 3, 98, 9, 76, 45, 4}, then the arrangement 998764543431
-* gives the largest value.
-***/
+ * Given an array of numbers, arrange them in a way that yields the largest
+ * value. For example, if the given numbers are {54, 546, 548, 60}, the
+ * arrangement 6054854654 gives the largest value. And if the given number
+ * are {1, 34, 3, 98, 9, 76, 45, 4}, then the arrangement 998764543431
+ * gives the largest value.
+ ***/
 
 #include <iostream>
 #include <vector>
@@ -14,8 +14,8 @@ using namespace std;
 
 string getLargestNumber(const vector<int> &array) {
     vector<string> stringArray(array.size());
-    for(auto it: array)
-        stringArray.push_back(to_string(it));
+    for(int i = 0; i < array.size(); i++)
+        stringArray[i] = to_string(array[i]);
     stable_sort(stringArray.begin(), stringArray.end(), [](string a, string b) {
         string temp = a;
         a.reserve(a.size() + b.size());
@@ -25,13 +25,23 @@ string getLargestNumber(const vector<int> &array) {
         return a.compare(b) > 0 ? true : false;
     });
     string str;
-    for(auto it: stringArray)
-        str.append(it);
+    string zero = "0";
+    int startIndex = 0;
+    if (stringArray[0].compare(zero) == 0) {
+        for ( int i = 1; i < stringArray.size(); i++) {
+            startIndex = i;
+            if (stringArray[0].compare(zero) != 0)
+                break;
+        }
+    }
+    for(int i = startIndex; i < stringArray.size(); i++)
+        str.append(stringArray[i]);
     return str;
 }
 
 int main() {
-    vector<int> array = {1,2,3,4,5,6,7,8,9,90,40};
+    vector<int> array = {1,2,3,4,5,6,7,8,9,90,40,0}; //{0,0,0,0};
     cout << getLargestNumber(array) << endl;
     return 0;
 }
+
